14b5874c67b331987261fca7af880b78f5d1fde0
Adam Giergun <adasss@poczta.onet.pl>
2022-07-08 16:42:17 +0200

    Implement authentication flow

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,24 +1,108 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.udacity.project4.R
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
+
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
  * signed in users to the RemindersActivity.
  */
+private const val TAG = "AuthenticationActivity"
+
 class AuthenticationActivity : AppCompatActivity() {
+
+    private val viewModel: AuthenticationViewModel by viewModels()
+    private val binding: ActivityAuthenticationBinding by lazy {
+        ActivityAuthenticationBinding.inflate(layoutInflater)
+    }
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        setContentView(binding.root)
 
-//          TODO: If the user was authenticated, send him to RemindersActivity
+        launchSignInFlow()
+
+        viewModel.authenticationState.observe(this) { authenticationState ->
+            when (authenticationState) {
+                AuthenticationViewModel.AuthenticationState.AUTHENTICATED -> {
+                    Intent(this, RemindersActivity::class.java).run {
+                        startActivity(this)
+                    }
+
+                }
+                else -> {
+                    binding.authText.text = getString(com.udacity.project4.R.string.authentication_failed)
+                }
+            }
+        }
 
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
+    }
+
+    private fun launchSignInFlow() {
+        // Give users the option to sign in / register with their email or Google account. If users
+        // choose to register with their email, they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent. We listen to the response of this activity with the
+        // SIGN_IN_RESULT_CODE code.
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == Activity.RESULT_OK) {
+            Log.i(
+                TAG,
+                "Successfully signed in user " +
+                        "${getDisplayName()}!"
+            )
+        } else {
+            Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+        }
+    }
+
+    private fun getDisplayName(): String {
+        return FirebaseAuth.getInstance().currentUser.let { user ->
+            user?.displayName.let {
+                var displayName = it
+                if (displayName == null || displayName == "") {
+                    val userInfoList = user?.providerData?.iterator()
+                    userInfoList?.forEach uil@{ userInfo ->
+                        displayName = userInfo.displayName
+                        if (displayName != null) return@uil
+                    }
+                }
+                if (displayName == null || displayName == "")
+                    user?.email ?: getString(com.udacity.project4.R.string.unknown_user)
+                else
+                    displayName
+            } ?: getString(com.udacity.project4.R.string.unknown_user)
+        }
     }
 }
