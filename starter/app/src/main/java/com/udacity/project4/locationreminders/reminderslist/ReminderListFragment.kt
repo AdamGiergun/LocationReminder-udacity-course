131783b55401eb5068bc828d8b8a9ce03d249ad3
Adam Giergun <adasss@poczta.onet.pl>
2022-07-13 18:37:59 +0200

    Add the logout implementation

--- starter/app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
@@ -1,10 +1,13 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
+import com.firebase.ui.auth.AuthUI
 import com.udacity.project4.R
+import com.udacity.project4.authentication.AuthenticationActivity
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentRemindersBinding
@@ -12,6 +15,7 @@ import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import com.udacity.project4.utils.setTitle
 import com.udacity.project4.utils.setup
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class ReminderListFragment : BaseFragment(), MenuProvider {
     //use Koin to retrieve the ViewModel instance
@@ -66,17 +70,26 @@ class ReminderListFragment : BaseFragment(), MenuProvider {
         val adapter = RemindersListAdapter {
         }
 
-//        setup the recycler view using the extension function
+//       TODO: setup the recycler view using the extension function
         binding.reminderssRecyclerView.setup(adapter)
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
         when (menuItem.itemId) {
             R.id.logout -> {
-//                TODO: add the logout implementation
+                logout()
             }
         }
         return false
+    }
+
+    private fun logout() {
+        AuthUI.getInstance()
+            .signOut(requireContext())
+            .addOnCompleteListener {
+                startActivity(Intent(requireActivity(), AuthenticationActivity::class.java))
+                requireActivity().finish()
+            }
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
