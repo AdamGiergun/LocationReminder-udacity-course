dcf6aa3d63655a7a269af4e29e6c8103589633e5
Adam Giergun <adasss@poczta.onet.pl>
2022-08-10 18:26:20 +0200

    Add getReminders implementation

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -8,9 +8,29 @@ class FakeDataSource : ReminderDataSource {
 
 //    TODO: Create a fake data source to act as a double to the real data source
 
-    override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
-    }
+    override suspend fun getReminders(): Result<List<ReminderDTO>> =
+            Result.Success(
+                listOf(
+                    ReminderDTO(
+                        "test1",
+                        "test1",
+                        "test1",
+                        0.0,
+                        0.0,
+                        true,
+                        "test1"
+                    ),
+                    ReminderDTO(
+                        "test2",
+                        "test2",
+                        "test2",
+                        180.0,
+                        180.0,
+                        false,
+                        "test2"
+                    )
+                )
+            )
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
         TODO("save the reminder")
@@ -18,6 +38,10 @@ class FakeDataSource : ReminderDataSource {
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
         TODO("return the reminder with the id")
+    }
+
+    override suspend fun setReminderState(id: String, isActive: Boolean) {
+        TODO("Not yet implemented")
     }
 
     override suspend fun deleteAllReminders() {
