a6a959774f07597634ee5b33fa65d588deefdd1d
Adam Giergun <adasss@poczta.onet.pl>
2022-08-23 16:00:46 +0200

    Initial version of RemindersActivityTest.kt

--- starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,16 +1,27 @@
 package com.udacity.project4
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -27,6 +38,7 @@ class RemindersActivityTest :
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+//    private val dataBindingIdlingResource = DataBindingIdlingResource()
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -49,7 +61,7 @@ class RemindersActivityTest :
                     get() as ReminderDataSource
                 )
             }
-            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single<ReminderDataSource> { RemindersLocalRepository(get()) }
             single { LocalDB.createRemindersDao(appContext) }
         }
         //declare a new koin module
@@ -65,7 +77,88 @@ class RemindersActivityTest :
         }
     }
 
+//    @Before
+//    fun registerIdlingResource() {
+//        IdlingRegistry.getInstance().register(
+//            EspressoIdlingResource.countingIdlingResource,
+//            dataBindingIdlingResource
+//        )
+//    }
+//
+//    @After
+//    fun unregisterIdlingResource() {
+//        IdlingRegistry.getInstance().unregister(
+//            EspressoIdlingResource.countingIdlingResource,
+//            dataBindingIdlingResource
+//        )
+//    }
 
-//    TODO: add End to End testing to the app
 
+    @Test
+    fun editReminder(): Unit = runBlocking {
+        val reminder = ReminderDataItem(
+            "title1",
+            "description1",
+            "location1",
+            0.0,
+            1.0,
+            true,
+            "test1"
+        )
+        reminder.run {
+            repository.saveReminder(
+                ReminderDTO(
+                    title,
+                    description,
+                    location,
+                    latitude,
+                    longitude,
+                    active,
+                    id
+                )
+            )
+        }
+
+        ActivityScenario.launch(RemindersActivity::class.java).run {
+            onView(withText("title1")).perform(click())
+            onView(withId(R.id.reminder_title))
+                .check(ViewAssertions.matches(withText("title1")))
+            onView(withId(R.id.reminder_description))
+                .check(ViewAssertions.matches(withText("description1")))
+            onView(withId(R.id.reminder_location))
+                .check(ViewAssertions.matches(withText("location1")))
+
+            close()
+        }
+    }
+
+    @Test
+    fun createOneReminder_deleteReminder() {
+
+        ActivityScenario.launch(RemindersActivity::class.java).run {
+
+            onView(withId(R.id.addReminderFAB)).perform(click())
+            onView(withId(R.id.reminder_title))
+                .perform(replaceText("TITLE2"))
+            onView(withId(R.id.reminder_description))
+                .perform(replaceText("DESCRIPTION2"))
+            onView(withId(R.id.saveReminder)).perform(click())
+
+            onView(withText("TITLE2")).perform(click())
+
+//            onView(withId(R.id.menu_delete)).perform(click())
+////            try {
+////
+////            } catch (e: NoMatchingViewException) {
+////                openActionBarOverflowOrOptionsMenu(getApplicationContext())
+////                onView(withText(R.string.menu_delete_task)).perform(click())
+////            }
+//
+//            onView(withId(R.id.menu_filter)).perform(click())
+//            onView(withText(R.string.nav_all)).perform(click())
+//            onView(withText("TITLE2")).check(doesNotExist())
+
+            close()
+        }
+    }
 }
